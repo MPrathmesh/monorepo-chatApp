5202fe8bb7b2a4c33885d9adb1995ed2e5636b35
Prathamesh More <moreprathamesh564@gmail.com>
2025-07-11 10:46:26 +0530

    Added initial chat page

---
+++ packages/ui/src/text-input.tsx
@@ -0,0 +1,24 @@
+interface PropType {
+    placeholder: string;
+    size : "big" | "small";
+}
+
+export function TextInput({
+    placeholder,
+    size,
+} : PropType) {
+    return (
+        <input
+            
+            type="text"
+            placeholder={placeholder}
+            style={{
+                padding: size === "big" ? "20" : "10",
+                margin : size === "big" ? "20" : "10",
+                borderColor : 'black',
+                borderWidth: '1',
+                borderRadius: '5',
+            }}
+        />
+    ); 
+}       
